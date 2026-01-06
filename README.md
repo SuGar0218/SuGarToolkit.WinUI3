3e50b9a56ad85ae4005f3356bfa2ba719badfc09
Sky_SuGar <wxk0218@icloud.com>
2026-01-06 10:10:15 +0800

    Add x64 and x86 compatibility for Get/Set WindowLongPtr

--- README.md
+++ README.md
@@ -24,3 +24,30 @@ ContentWindow is somewhat like Window ([System.Windows.Window](https://learn.mic
 
 <img width="1918" height="1018" alt="ContentWindow" src="https://github.com/user-attachments/assets/f32e6a7a-026c-4c75-a781-0e7a02218223" />
 
+## Build
+
+To build for 64-bit system, please define constant `SUGAR_TOOLKIT_X64`. One feasible approach is adding the config below to csproj.
+
+``` xml
+<PropertyGroup Condition="'$(Platform)'=='x64'">
+    <DefineConstants>$(DefineConstants);SUGAR_TOOLKIT_X64</DefineConstants>
+</PropertyGroup>
+```
+
+Since this library references Windows APIs,
+
+some only works on 64-bit system: `GetWindowLongPtr`, `SetWindowLongPtr`;
+
+some are designed for 32-bit system: `GetWindowLong`, `SetWindowLong`;
+
+there is code like this:
+
+``` C#
+#if SUGAR_TOOLKIT_X64
+    private static nint GetWindowLongPtr(HWND hWnd, WINDOW_LONG_PTR_INDEX nIndex) => PInvoke.GetWindowLongPtr(hWnd, nIndex);
+    private static nint SetWindowLongPtr(HWND hWnd, WINDOW_LONG_PTR_INDEX nIndex, nint dwNewLong) => PInvoke.SetWindowLongPtr(hWnd, nIndex, dwNewLong);
+#else
+    private static int GetWindowLongPtr(HWND hWnd, WINDOW_LONG_PTR_INDEX nIndex) => PInvoke.GetWindowLong(hWnd, nIndex);
+    private static int SetWindowLongPtr(HWND hWnd, WINDOW_LONG_PTR_INDEX nIndex, int dwNewLong) => PInvoke.SetWindowLong(hWnd, nIndex, dwNewLong);
+#endif
+```
--- SuGarToolkit.WinUI3.Controls.Windows/WindowStyleHelper.cs
+++ SuGarToolkit.WinUI3.Controls.Windows/WindowStyleHelper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Windows.Win32;
+﻿using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.WindowsAndMessaging;
 
@@ -12,83 +6,91 @@ namespace SuGarToolkit.WinUI3.Controls.Windows;
 
 internal class WindowStyleHelper
 {
-	public WindowStyleHelper(HWND hwnd)
-	{
-		_hwnd = hwnd;
-	}
+    public WindowStyleHelper(HWND hwnd)
+    {
+        _hwnd = hwnd;
+    }
 
-	private readonly HWND _hwnd;
+    private readonly HWND _hwnd;
 
-	private bool _canMinimize;
-	private bool _canMaximize;
+    private bool _canMinimize;
+    private bool _canMaximize;
 
-	public bool CanMinimize
-	{
-		get => _canMinimize;
-		set
-		{
-			if (_canMinimize == value)
-				return;
+    public bool CanMinimize
+    {
+        get => _canMinimize;
+        set
+        {
+            if (_canMinimize == value)
+                return;
 
-			if (value)
-			{
-				EnableMinimize();
-			}
-			else
-			{
-				DisableMinimize();
-			}
-		}
-	}
+            if (value)
+            {
+                EnableMinimize();
+            }
+            else
+            {
+                DisableMinimize();
+            }
+        }
+    }
 
-	public bool CanMaximize
-	{
-		get => _canMaximize;
-		set
-		{
-			if (_canMaximize == value)
-				return;
+    public bool CanMaximize
+    {
+        get => _canMaximize;
+        set
+        {
+            if (_canMaximize == value)
+                return;
 
-			if (value)
-			{
-				EnableMaximize();
-			}
-			else
-			{
-				DisableMaximize();
-			}
-		}
-	}
+            if (value)
+            {
+                EnableMaximize();
+            }
+            else
+            {
+                DisableMaximize();
+            }
+        }
+    }
 
-	public nint EnableMinimize()
-	{
-		_canMinimize = true;
-		nint style = PInvoke.GetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
-		style |= WindowStyles.WS_MINIMIZEBOX;
-		return PInvoke.SetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
-	}
+    public nint EnableMinimize()
+    {
+        _canMinimize = true;
+        var style = GetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        style |= WindowStyles.WS_MINIMIZEBOX;
+        return SetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
+    }
 
-	public nint DisableMinimize()
-	{
-		_canMinimize = false;
-		nint style = PInvoke.GetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
-		style &= ~WindowStyles.WS_MINIMIZEBOX;
-		return PInvoke.SetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
-	}
+    public nint DisableMinimize()
+    {
+        _canMinimize = false;
+        var style = GetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        style &= ~WindowStyles.WS_MINIMIZEBOX;
+        return SetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
+    }
 
-	public nint EnableMaximize()
-	{
-		_canMaximize = true;
-		nint style = PInvoke.GetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
-		style |= WindowStyles.WS_MAXIMIZEBOX;
-		return PInvoke.SetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
-	}
+    public nint EnableMaximize()
+    {
+        _canMaximize = true;
+        var style = GetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        style |= WindowStyles.WS_MAXIMIZEBOX;
+        return SetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
+    }
 
-	public nint DisableMaximize()
-	{
-		_canMaximize = false;
-		nint style = PInvoke.GetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
-		style &= ~WindowStyles.WS_MAXIMIZEBOX;
-		return PInvoke.SetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
-	}
+    public nint DisableMaximize()
+    {
+        _canMaximize = false;
+        var style = GetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        style &= ~WindowStyles.WS_MAXIMIZEBOX;
+        return SetWindowLongPtr(_hwnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE, style);
+    }
+
+#if SUGAR_TOOLKIT_X64
+    private static nint GetWindowLongPtr(HWND hWnd, WINDOW_LONG_PTR_INDEX nIndex) => PInvoke.GetWindowLongPtr(hWnd, nIndex);
+    private static nint SetWindowLongPtr(HWND hWnd, WINDOW_LONG_PTR_INDEX nIndex, nint dwNewLong) => PInvoke.SetWindowLongPtr(hWnd, nIndex, dwNewLong);
+#else
+    private static int GetWindowLongPtr(HWND hWnd, WINDOW_LONG_PTR_INDEX nIndex) => PInvoke.GetWindowLong(hWnd, nIndex);
+    private static int SetWindowLongPtr(HWND hWnd, WINDOW_LONG_PTR_INDEX nIndex, int dwNewLong) => PInvoke.SetWindowLong(hWnd, nIndex, dwNewLong);
+#endif
 }
