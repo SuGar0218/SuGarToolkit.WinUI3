3c8cddf5ac28ef862415c8fda5acd7bee40c66a3
Sky_SuGar <wxk0218@icloud.com>
2026-05-15 00:03:45 +0800

    Update LiquidGlass-Like interaction sample page

--- SuGarToolkit.WinUI3.GalleryApp/Views/LiquidGlassLike/LiquidGlassLikeInteractionPage.xaml.cs
+++ SuGarToolkit.WinUI3.GalleryApp/Views/LiquidGlassLike/LiquidGlassLikeInteractionPage.xaml.cs
@@ -1,4 +1,8 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+
+using SuGarToolkit.WinUI3.Controls.Decorators;
+using SuGarToolkit.WinUI3.Helpers;
 
 namespace SuGarToolkit.WinUI3.GalleryApp.Views.LiquidGlassLike;
 
@@ -7,5 +11,26 @@ public sealed partial class LiquidGlassLikeInteractionPage : Page
     public LiquidGlassLikeInteractionPage()
     {
         InitializeComponent();
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Button button = new()
+                {
+                    Width = 56,
+                    Height = 56,
+                    Content = new TextBlock
+                    {
+                        Text = (i * 3 + j + 1).ToString(),
+                        FontSize = 24
+                    }
+                };
+                CapsuleShapeHelper.SetShapeIntoCapsule(button, true);
+                LiquidGlassLikeInteractionDecorator decorator = new() { Content = button };
+                Grid.SetRow(decorator, i);
+                Grid.SetColumn(decorator, j);
+                PART_DialGrid.Children.Add(decorator);
+            }
+        }
     }
 }
