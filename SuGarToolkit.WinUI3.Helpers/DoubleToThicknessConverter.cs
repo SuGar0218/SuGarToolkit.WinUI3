5bdcfa176036fb4b4860cf04c7db65131ddb6531
Sky_SuGar <wxk0218@icloud.com>
2026-06-28 20:14:23 +0800

    Add double value to any sides of thickness converter

---
+++ SuGarToolkit.WinUI3.Helpers/DoubleToThicknessConverter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+using System;
+
+namespace SuGarToolkit.WinUI3.Helpers;
+
+public partial class DoubleToThicknessConverter : IValueConverter
+{
+    public static Thickness Convert(double value, ThicknessSides sides)
+    {
+        ThicknessSides[] allSides = [ThicknessSides.Left, ThicknessSides.Top, ThicknessSides.Right, ThicknessSides.Bottom];
+        double[] result = new double[4];
+        for (int i = 0; i < 4; i++)
+        {
+            result[i] = sides.HasFlag(allSides[i]) ? value : 0;
+        }
+        return new Thickness(result[0], result[1], result[2], result[3]);
+    }
+
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is not double doubleValue)
+            return value;
+
+        return parameter switch
+        {
+            ThicknessSides sides => Convert(doubleValue, sides),
+            int intValue => Convert(doubleValue, intValue.ToThicknessSides()),
+            _ => Convert(doubleValue, ThicknessSides.Left | ThicknessSides.Top | ThicknessSides.Right | ThicknessSides.Bottom),
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ SuGarToolkit.WinUI3.Helpers/ThicknessSides.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SuGarToolkit.WinUI3.Helpers;
+
+[Flags]
+public enum ThicknessSides
+{
+    None = 0b0000,
+    Left = 0b1000,
+    Top = 0b0100,
+    Right = 0b0010,
+    Bottom = 0b00001
+}
+
+public static class ThicknessSidesExtensions
+{
+    public static ThicknessSides ToThicknessSides(this int value)
+    {
+        ThicknessSides sides = ThicknessSides.None;
+        foreach (ThicknessSides side in Enum.GetValues<ThicknessSides>())
+        {
+            if (((int) side & value) != 0)
+            {
+                sides |= side;
+            }
+        }
+        return sides;
+    }
+}
